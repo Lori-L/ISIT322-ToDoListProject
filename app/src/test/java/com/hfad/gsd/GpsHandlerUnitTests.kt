31228456c3390f3a11f4cef250b998655893d89d
Lori-L <77129738+Lori-L@users.noreply.github.com>
2023-06-12 22:22:49 -0700

    Populating GpsHandlerUnitTests.kt

--- app/src/test/java/com/hfad/gsd/GpsHandlerUnitTests.kt
+++ app/src/test/java/com/hfad/gsd/GpsHandlerUnitTests.kt
@@ -1,4 +1,21 @@
 package com.hfad.gsd
 
+import android.util.Log
+import org.junit.Test
+
+import org.junit.Assert.*
+
 class GpsHandlerUnitTests {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun GPS1() {
+        println("start")
+        var gps = GpsHandler()
+        println(gps.latitude.toString() + " " + gps.longitude.toString())
+        assertNull(gps.latitude)
+    }
 }
