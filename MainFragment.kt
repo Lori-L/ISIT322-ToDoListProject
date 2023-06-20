b07f61bf7d96b3480b88a83db066d57ca6252682
Marcus <55485519+lazarom37@users.noreply.github.com>
2023-06-19 22:29:29 -0700

    Notification and Time
    Created working notifications that now show accurate current time as a means of mocking time display

---
+++ MainFragment.kt
@@ -0,0 +1,35 @@
+package com.hfad.gsd
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.content.Context
+import androidx.navigation.findNavController
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val viewButton = view.findViewById<Button>(R.id.view)
+        val notifyButton = view.findViewById<Button>(R.id.notification)
+        val service = context?.let { CounterNotificationService(it) }
+        // ^^^ was originally val service = CounterNotificationService(context),
+
+        viewButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_mainFragment_to_taskFragment)
+        }
+
+        notifyButton.setOnClickListener {
+            if (service != null) {
+                service.showNotification()
+            }
+        }
+        return view
+    }
+}
